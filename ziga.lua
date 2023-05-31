ad99d4446e5008ceb89f5e1d97860611fe01d303
Petr <98262200+kahendrik@users.noreply.github.com>
2023-05-31 17:07:16 +0300

    Update ziga.lua

--- ziga.lua
+++ ziga.lua
@@ -1,3 +1,6 @@
+script_name("Ziga By Yakowlev")
+script_version("31.05.2023")
+
 local ziga = false
 function main()
 	sampAddChatMessage("[ZIGA]{6495ED} Зига успешно загружена. {FFFFFF}Чтобы её кинуть, введите: {F08080}/ziga", 0xF08080)
@@ -5,7 +8,7 @@ function main()
 	while true do
 		if ziga then
 			taskPlayAnim(PLAYER_PED, "Pointup_loop", "ON_LOOKERS", 12, true, false, false, false, -1)
-			sampAddChatMessage("[Ziga] Зига успешно кинута. Чтобы остановить анимку нажми F.")
+			sampAddChatMessage("[ZIGA] Зига успешно кинута. Чтобы остановить анимку нажми F.")
 			if wasKeyPressed(0x46) and not sampIsCursorActive() then
 				ziga = false
 				clearCharTasks(1)
